ce6c87cc01c9baaa94e547db4b2f4fadcdb34ace
Kevin M Ashton <kevin.m.ashton@gmail.com>
2023-12-20 11:05:27 -0700

    Quick save

---
+++ quiz.ts
@@ -0,0 +1,15 @@
+interface QuizItem {
+  concept: string;
+  description: string;
+}
+
+const quiz: QuizItem[] = [
+  {
+    concept: "domain registrar",
+    description: "...",
+  },
+  {
+    concept: "cloud provider",
+    description: "",
+  },
+];
